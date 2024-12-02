e411eabb2165b6ccabc1fdc4418aa962fbe6e9ab
Rodney Lab <codesign@rodneylab.com>
2024-12-02 08:56:41 +0000

    build(dependencies): 🧱 update packages
    ┌───────────────────────────────────────┬─────────┬─────────┐
    │ Package                               │ From    │ To      │
    ├───────────────────────────────────────┼─────────┼─────────┤
    │ husky (dev)                           │ 9.1.4   │ 9.1.7   │
    ├───────────────────────────────────────┼─────────┼─────────┤
    │ @commitlint/config-conventional (dev) │ 19.2.2  │ 19.6.0  │
    ├───────────────────────────────────────┼─────────┼─────────┤
    │ astro (dev)                           │ 4.13.2  │ 4.16.16 │
    ├───────────────────────────────────────┼─────────┼─────────┤
    │ commitlint (dev)                      │ 19.4.0  │ 19.6.0  │
    ├───────────────────────────────────────┼─────────┼─────────┤
    │ prettier (dev)                        │ 3.3.3   │ 3.4.1   │
    ├───────────────────────────────────────┼─────────┼─────────┤
    │ stylelint (dev)                       │ 16.8.1  │ 16.11.0 │
    ├───────────────────────────────────────┼─────────┼─────────┤
    │ typescript (dev)                      │ 5.5.4   │ 5.7.2   │
    ├───────────────────────────────────────┼─────────┼─────────┤
    │ vite-node (dev)                       │ 2.0.5   │ 2.1.6   │
    ├───────────────────────────────────────┼─────────┼─────────┤
    │ @astrojs/check (dev)                  │ 0.9.2   │ 0.9.4   │
    └───────────────────────────────────────┴─────────┴─────────┘

--- src/content/methods/_with.ts
+++ src/content/methods/_with.ts
@@ -5,14 +5,9 @@ const now = Temporal.Now.plainDateTimeISO().toLocaleString('en-GB', {
 	timeStyle: 'long',
 });
 
-const nowString = now.toLocaleString('en-GB', {
-	dateStyle: 'full',
-	timeStyle: 'long',
-});
-
 const result = Temporal.Now.plainDateTimeISO()
 	.add({ months: 1 })
 	.with({ hour: 23, minute: 59, second: 0 })
 	.toLocaleString('en-GB', { dateStyle: 'full', timeStyle: 'long' });
 
-export { nowString as now, result };
+export { now, result };
